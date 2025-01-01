06f770af6971e989f4e592d93145021a8689cb7e
Spencer Vaughn <svaughn57@gmail.com>
2025-01-01 16:41:43 -0600

    Add Makefile and setup script for STM32 project with build configuration

---
+++ setup-macos.sh
@@ -0,0 +1,6 @@
+
+ brew install --cask gcc-arm-embedded 
+
+cd ..
+git clone --recursive  --depth 1 --branch v1.7.0 https://github.com/STMicroelectronics/STM32CubeH7.git
+git clone https://github.com/arduino/ArduinoCore-mbed.git
